39cb232b4839e20cd77cfd7801d8ed47182e2799
Davi Marques <davmarques27@gmail.com>
2024-04-02 00:52:04 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('.ri-apps-line')
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle('.ri-close-large-fill')
+    navbar.classList.toggle('open')
+}
+
